0a9e5d804589b0fcbd67c553c8cd7ef4e5348f9e
Timur Mukhametzyanov <bratov.tima@mail.ru>
2023-03-04 14:06:11 +0400

    first ver header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
-    <div>
+    <div className={layout.wrap}>
       <header className={layout.header}>
         <div className={layout.wrap_container}>
           <Navbar />
