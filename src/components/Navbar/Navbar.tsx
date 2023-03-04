6dbe60ba0f1d3ddcac0496fd063a26ea83ed15dc
Timur Mukhametzyanov <bratov.tima@mail.ru>
2023-03-04 23:23:52 +0400

    added burger-menu, full-adaptive and search-input

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,36 +1,63 @@
-import React from "react";
-import app from "../App/app.module.scss";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import navbar from "./navbar.module.scss";
 import search_icon from "../../assets/icons/search.svg";
-import burger_icon from "../../assets/icons/free-icon-bar-7131131.png";
 import { CustomLink } from "./CustomLink";
-export const Navbar = () => {
+export const Navbar: React.FC = () => {
+  const [navMenu, setNavMenu] = useState(false);
+  const changeVisibilityBurger = () => {
+    setNavMenu((prev) => !prev);
+  }
   return (
     <div className={navbar.navigation_container}>
-      <nav className={navbar.menu}>
-        <ul>
-          <CustomLink to="/">Главная</CustomLink>
+      <nav className={navbar.nav_menu}>
+        <ul
+          className={
+            navMenu
+              ? [navbar.menu, navbar.active].join(" ")
+              : [navbar.menu].join(" ")
+          }
+        >
+          <CustomLink to="/"><div onClick={changeVisibilityBurger}>Главная</div></CustomLink>
 
-          <CustomLink to="/blog">Блог</CustomLink>
+          <CustomLink to="/blog"><div onClick={changeVisibilityBurger}>Блог</div></CustomLink>
 
-          <CustomLink to="/game">Игра</CustomLink>
+          <CustomLink to="/game"><div onClick={changeVisibilityBurger}>Игра</div></CustomLink>
 
-          <CustomLink to="/library">Библиотека</CustomLink>
+          <CustomLink to="/library"><div onClick={changeVisibilityBurger}>Библиотека</div></CustomLink>
 
-          <CustomLink to="/tournament">Турнир</CustomLink>
+          <CustomLink to="/tournament"><div onClick={changeVisibilityBurger}>Турнир</div></CustomLink>
 
-          <CustomLink to="/gallery">Галерея</CustomLink>
+          <CustomLink to="/gallery"><div onClick={changeVisibilityBurger}>Галерея</div></CustomLink>
         </ul>
       </nav>
-
       <div className={navbar.right_nav}>
-        {/* ПОИСК ЗДЕСЬ ИНПУТ И ТД ИЗНАЧАЛЬНО ОНЛИ ИКОНА ПО НАЖАТИЮ ПОЯВЛЯЕТСЯ ИНПУТ*/}
-        {/* Бургер думаю тут будет но хз посмотришь сам (именно иконка)*/}
+        <div className={navbar.wrap_input}>
+          <input
+            type="text"
+            name="search"
+            className={navbar.input_search}
+            placeholder="Поиск..."
+          />
+          <a href="#" className={navbar.search_btn}>
+            <img src={search_icon} alt="search" />
+          </a>
+        </div>
         <nav className={navbar.auth_btns}>
           <Link to={"/authorization"}>Войти</Link>
-          <Link to={"/registration"}>Зарегистрироваться</Link>
         </nav>
+        <div
+          onClick={() => setNavMenu(!navMenu)}
+          className={
+            navMenu
+              ? [navbar.burger_menu, navbar.active_burger].join(" ")
+              : [navbar.burger_menu].join(" ")
+          }
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
   );
