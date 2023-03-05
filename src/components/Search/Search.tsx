4d4ce88d4cde9195911e15070e0be5a106927e8d
Дмитрий Чанков <chankovbelka123@yandex.ru>
2023-03-05 17:36:18 +0400

    fix search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,19 +1,54 @@
-import React, { useRef, useState } from "react";
+import React, {
+  ChangeEvent,
+  MouseEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import search from "./search.module.scss";
 import search_icon from "../../assets/icons/search-svgrepo-com.svg";
 export const Search: React.FC = () => {
   const [searchIsOpen, setSearchIsOpen] = useState(false);
-  const input = useRef(null);
+  const [inputValue, setInputValue] = useState("");
+  const inputDOM = useRef<HTMLInputElement>(null);
+  const inputContainerDOM = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: Event) {
+      if (
+        inputContainerDOM.current &&
+        !inputContainerDOM.current.contains(event.target as Node)
+      ) {
+        closeSearch();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [inputDOM]);
+
   const openSearch = () => {
-    setSearchIsOpen((prev) => !prev);
-    input.current.focus();
+    setSearchIsOpen(true);
+    inputDOM?.current?.focus();
   };
+
+  const closeSearch = () => {
+    setSearchIsOpen(false);
+  };
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <div className={search.wrap_input}>
+    <div ref={inputContainerDOM} className={search.wrap_input}>
       <input
-        ref={input}
+        ref={inputDOM}
         type="text"
         name="search"
+        value={inputValue}
+        onChange={onChangeHandler}
         className={
           searchIsOpen
             ? [search.input_search, search.active_input].join(" ")
@@ -21,9 +56,9 @@ export const Search: React.FC = () => {
         }
         placeholder="Поиск..."
       />
-      <a href="#" className={search.search_btn} onClick={openSearch}>
+      <button className={search.search_btn} onClick={openSearch}>
         <img src={search_icon} alt="search" />
-      </a>
+      </button>
     </div>
   );
 };
