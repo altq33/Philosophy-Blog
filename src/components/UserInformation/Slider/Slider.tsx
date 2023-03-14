34e787793ce1b2565f7d3a656d409e0f5f686048
Дмитрий Чанков <chankovbelka123@yandex.ru>
2023-03-14 23:13:39 +0400

    fix slider

--- src/components/UserInformation/Slider/Slider.tsx
+++ src/components/UserInformation/Slider/Slider.tsx
@@ -6,31 +6,20 @@ export const Slider: React.FC<ISlider> = ({
   right_value,
   position,
 }) => {
-  const [curWidth, setCurWidth] = useState(0);
-  const line = useRef<HTMLDivElement>(null);
-
-  const handleResize = () => {
-    setCurWidth(line.current?.offsetWidth!! / 10);
-  };
-// FIXME: вынести логику повыше шобы был один ивент листенер 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    setCurWidth(line.current?.offsetWidth!! / 10);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div className={slider.parent}>
       <div className={slider.titles}>
         <span>{left_value}</span>
         <span>{right_value}</span>
       </div>
-      <div ref={line} className={slider.slider}>
+      <div className={slider.slider}>
         <div
           className={slider.square}
-          style={{ left: `${position * curWidth - 7.5}px` }}
+          style={{
+            left: `${position * 10}%`,
+            [position == 0 ? "transform" : ""]: "none",
+            [position == 10 ? "transform" : ""]: "translate(-100%)",
+          }}
         ></div>
       </div>
     </div>
